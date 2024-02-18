802e4ab76cd0c3f079b4ffb3e1b02c8fa58488ed
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-02-18 18:54:54 +0530

    photo changed
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -7,14 +7,14 @@ export default function Intro() {
         <img
           src='/poster.jpeg'
           alt=''
-          className='rounded-xl mb-4 h-[470px] object-top w-full object-cover'
+          className='rounded-xl mb-4 h-[75%] w-full object-top  object-cover'
         />
         <h1 className='text-2xl text-[#292c39] sm:text-5xl mt-8 font-semibold'>
           Web Study Jams - GDSC GITAM
         </h1>
-        <h2 className='text-lg text-[#656c73] sm:text-xl mt-8 font-semibold inline-flex items-center space-x-2'>
+        <div className='text-lg text-[#656c73] sm:text-xl mt-8 font-semibold inline-flex items-center space-x-2'>
           <BsFillPeopleFill /> <h2>100+ Members</h2>
-        </h2>
+        </div>
       </div>
       <hr />
     </div>
