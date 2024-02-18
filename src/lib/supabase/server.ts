17696ad9caca3eabc7aeb3c5d2ccad1607c85152
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-02-18 23:51:56 +0530

    Add Supabase Google OAuth

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,7 +1,26 @@
-import { createClient } from '@supabase/supabase-js'
+"use server";
 
-const PROJECT_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const PUBLIC_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
-const supabase = createClient(PROJECT_URL, PUBLIC_ANON_KEY);
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { cookies } from "next/headers";
 
-export default supabase;
+export default async function createSupabaseServerClient() {
+  const cookieStore = cookies();
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          cookieStore.set({ name, value, ...options });
+        },
+        remove(name: string, options: CookieOptions) {
+          cookieStore.set({ name, value: "", ...options });
+        },
+      },
+    },
+  );
+}
