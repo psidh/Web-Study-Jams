f966c87e11319c3fd3cdeebc1f74d33a380c5ff1
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-02-19 20:11:16 +0530

    added `<iframe>`
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,7 +6,7 @@ export default function Projects() {
       <h1 className='text-2xl text-[#292c39] sm:text-5xl mt-8 font-semibold'>
         Projects Showcase
       </h1>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 my-8'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 my-8'>
         <ProjectsCard />
         <ProjectsCard />
         <ProjectsCard />
--- src/components/ProjectsCard.tsx
+++ src/components/ProjectsCard.tsx
@@ -6,9 +6,14 @@ export default function ProjectsCard() {
       className='flex flex-col items-start justify-center rounded-xl border cursor-pointer
     border-[#cecece]  duration-200 transition-all hover:shadow-2xl p-4'
     >
-      <img src='/project.png' alt='' className='object-cover mb-2 rounded-lg' />
-      <h1 className='text-2xl my-2 '>Project Name</h1>
-      <h2 className='text-lg my-2 font-semibold'>Sidharth</h2>
+      {/* <img src='/project.png' alt='' className='object-cover mb-2 rounded-lg' /> */}
+      <iframe
+        src='https://monicagoud.in'
+        className='w-full aspect-video rounded-xl '
+        scrolling='false'
+      />
+      <h1 className='text-2xl my-2 '>Monica Goud</h1>
+      <h2 className='text-lg my-2 font-semibold'>Startup Coach</h2>
       <p className='text-[#888888] text-justify'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo,
         obcaecati? Magni adipisci accusamus alias blanditiis, veniam reiciendis
