9dc9e879d836fd8cff15d3b3e22157cd4370058b
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-02-22 11:29:35 +0530

    UI resources added
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

---
+++ src/components/GoogleSlides.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const GoogleSlides = () => {
+  return (
+    <div>
+      <iframe
+        src='https://docs.google.com/presentation/d/e/2PACX-1vRbLuLA6NOjiULH2DhxHojOdpHgn3MAULxNVk-f3E7-rhOiAUZ0t3Y7qtrjHktgIraP3GMb42f8sOWi/embed?start=true&loop=true&delayms=1000'
+        width={1200}
+        height={700}
+        className='my-16'
+      ></iframe>
+    </div>
+  );
+};
+
+export default GoogleSlides;
