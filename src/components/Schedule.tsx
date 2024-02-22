bc0cd4fe8b5fea591a405655bab827f30534b09c
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-02-22 11:41:09 +0530

    README.md updated
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -1,11 +1,8 @@
-import GoogleSlides from './GoogleSlides';
-import ProjectsCard from './ProjectsCard';
-
 export default function Schedule() {
   return (
     <section className='md:px-32 px-6 md:py-12 py-6 flex flex-col item-center justify-center'>
       <h1 className='text-2xl text-[#292c39] sm:text-5xl mt-8 font-semibold'>
-        Week 1
+        Schedule
       </h1>
     </section>
   );
