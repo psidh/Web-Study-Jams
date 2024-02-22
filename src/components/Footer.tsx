4650761c5b8d1e656d3953534a66e6e14cc53754
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-02-22 15:18:56 +0530

    schedule added
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ import {
 } from 'react-icons/ri';
 export default function Footer() {
   return (
-    <div className='flex-col md:grid md:grid-cols-12 md:px-32 px-6 md:py-12 py-6  border-t border-[#d3d3d3] shadow-xl'>
+    <div className='flex-col items-start justify-start md:grid md:grid-cols-12 md:px-32 px-6 md:py-12 py-6  border-t border-[#d3d3d3] shadow-xl'>
       <img src='/footerlogo.svg' alt='' className='md:col-span-3 mb-8' />
       <div className='flex flex-col items-start justify-center col-span-5 mx-8'>
         <a href='/' className='inline-flex mx-2 items-center'>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,7 +3,7 @@ import ProjectsCard from './ProjectsCard';
 export default function Projects() {
   return (
     <section className='md:px-32 px-6 md:py-12 py-6'>
-      <h1 className='text-2xl text-[#292c39] sm:text-5xl  font-semibold'>
+      <h1 className='text-3xl my-16 text-[#292c39] sm:text-5xl  font-semibold'>
         Projects Showcase
       </h1>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 my-8'>
