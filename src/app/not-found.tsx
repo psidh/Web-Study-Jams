c49c47d2ba73a678144858706fa8cc885b176e38
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-02-23 13:45:28 +0530

    not-found.tsx changed

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -15,7 +15,7 @@ function NotFound() {
        hover:-translate-x-8 border flex justify-center items-center px-4 py-2 bg-white text-sky-600 hover:bg-sky-600 hover:text-white'
       >
         <IoArrowBackCircleOutline className='mr-2 inline-block' />
-        Take Me Home
+        Home
       </a>
       <div className='mt-8 text-center text-white text-opacity-60'>
         <p>While you&#39;re here, enjoy this funny 404 page. 🎉</p>
