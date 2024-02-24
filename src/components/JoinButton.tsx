213b49b3769793bc050ff7e6c034add6907f8061
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-02-24 13:47:54 +0530

    `JoinButton.tsx` changed

--- src/components/JoinButton.tsx
+++ src/components/JoinButton.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 export default function JoinButton() {
   return (
-    <div className='flex-col justify-center items-center md:px-32 px-6 py-6 '>
-      <h1 className='text-lg my-2 font-normal'>Excited about Study-Jams ?</h1>
-      <a href='/login' className='text-lg bg-blue-500 rounded-full text-white px-8 py-2 my-8 font-normal'>
-        Join Now
-      </a>
-    </div>
+    <>
+      <hr />
+      <div className='flex-col justify-center items-center my-16 md:px-32 px-6 py-6 text-center'>
+        <h1 className='text-lg my-2 font-normal'>Excited about Study-Jams ?</h1>
+        <a
+          href='/login'
+          className='text-lg bg-blue-500 rounded-full text-white px-8 py-2 my-8 font-normal'
+        >
+          Join Now
+        </a>
+      </div>
+    </>
   );
 }
