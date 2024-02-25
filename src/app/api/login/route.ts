15098b6e6bd83c2f84a2285c7fdbea477cbe9ab5
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-02-25 20:29:14 +0530

    add url() in /login/page.tsx

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -2,6 +2,18 @@ import createSupabaseServerClient from "@/lib/supabase/server";
 import { NextResponse } from "next/server";
 
 
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    'http://localhost:3000/'
+  
+  url = url.includes('http') ? url : `https://${url}`
+  
+  url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
+  return url
+}
+
 export async function POST() {
   const supabase = await createSupabaseServerClient();
   const { data, error } = await supabase.auth.signInWithOAuth({
@@ -11,6 +23,7 @@ export async function POST() {
         access_type: "offline",
         prompt: "consent",
       },
+      redirectTo : getURL(),
     },
   });
 
